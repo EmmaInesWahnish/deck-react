3893200a56062fe6b09e19924cb1e6118621882c
Emma <emma.wahnish@groovinads.com>
2022-04-16 19:50:05 -0300

    simple login

--- src/authentication/Login.jsx
+++ src/authentication/Login.jsx
@@ -1,8 +1,9 @@
 import img2 from "../assets/login-background.png";
-import { useAuthContext } from "../context/AuthContextProvider.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from "firebase/auth";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase.config.js";
+
 
 const sidebarBackground = {
 	backgroundImage: "url(" + img2 + ")",
@@ -16,21 +17,43 @@ const Login = () => {
 	const [registerPassword, setRegisterPassword] = useState("");
 	const [loginEmail, setLoginEmail] = useState("");
 	const [loginPassword, setLoginPassword] = useState("");
-
-	const { register, login, logout } = useAuthContext();
-
 	const [user, setUser] = useState({});
 
-	onAuthStateChanged(auth, (currentUser) => {
-		setUser(currentUser);
-	})
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser)
+		});
+		return unsubscribe;
+	}, []);
 
-	function newUser() {
-		register(registerEmail, registerPassword);
+	const register = async () => {
+		try {
+			const user = await createUserWithEmailAndPassword(
+				auth,
+				registerEmail,
+				registerPassword
+			)
+			console.log(user)
+		} catch (error) {
+			console.log(error.message);
+		}
 	}
 
-	function existingUser() {
-		login(loginEmail, loginPassword);
+	const login = async () => {
+		try {
+			const user = await signInWithEmailAndPassword(
+				auth,
+				loginEmail,
+				loginPassword
+			)
+			console.log(user)
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
+
+	const logout = async () => {
+		await signOut(auth);
 	}
 
 	return (
@@ -43,9 +66,9 @@ const Login = () => {
 						<h3>Register with email and password</h3>
 					</div>
 					<div className="d-flex">
-						<input name="newu" placeholder="email..." onChange={(event) => { setRegisterEmail(event.target.value) }} />
-						<input name="newp" placeholder="password..." onChange={(event) => { setRegisterPassword(event.target.value) }} />
-						<button className="btn btn-success" onClick={newUser()}>Create User</button>
+						<input placeholder="email..." onChange={(event) => { setRegisterEmail(event.target.value) }} />
+						<input placeholder="password..." onChange={(event) => { setRegisterPassword(event.target.value) }} />
+						<button className="btn btn-success" onClick={register}>Create User</button>
 					</div>
 				</div>
 				<div className="m-5">
@@ -53,9 +76,9 @@ const Login = () => {
 						<h3>Login with email and password</h3>
 					</div>
 					<div className="d-flex">
-						<input name="existu" placeholder="email..." onChange={(event) => { setLoginEmail(event.target.value) }} />
-						<input name="existp" placeholder="password..." onChange={(event) => { setLoginPassword(event.target.value) }} />
-						<button className="btn btn-light" onClick={existingUser}>Login</button>
+						<input placeholder="email..." onChange={(event) => { setLoginEmail(event.target.value) }} />
+						<input placeholder="password..." onChange={(event) => { setLoginPassword(event.target.value) }} />
+						<button className="btn btn-light" onClick={login}>Login</button>
 					</div>
 				</div>
 				<div className="m-5">
--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,7 +1,5 @@
 import { createContext, useContext } from "react";
 import { useLocalStorage } from "../helpers/useLocalStorage.js"
-import {auth} from "../firebase.config.js";
-import { createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from "firebase/auth";
 
 const AuthContext = createContext([]);
 
@@ -11,41 +9,10 @@ function AuthContextProvider({ children }) {
 
     const [authList, setAuthList] = useLocalStorage("navegation", []);
 
-    const register = async (email, password)=>{
-        try{
-            const user = await createUserWithEmailAndPassword(
-                auth,
-                email,
-                password
-            )
-        } catch (error){
-            console.log(error.message);
-        }
-    }
-
-    const login = async (email,password)=>{
-        try{
-            const user = await signInWithEmailAndPassword(
-                auth,
-                email,
-                password
-            )
-        } catch (error){
-            console.log(error.message);
-        }
-    }
-
-    const logout = async ()=>{
-        await signOut(auth);
-    }
-
     return (
         <AuthContext.Provider value={{
             authList,
             setAuthList,
-            register,
-            login,
-            logout
         }}>
             {children}
         </AuthContext.Provider>
