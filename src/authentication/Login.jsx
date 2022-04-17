c8ff96c653af6d967e61fbf070c5525abe49043f
Emma <emma.wahnish@groovinads.com>
2022-04-16 22:29:00 -0300

    styling

--- src/authentication/Login.jsx
+++ src/authentication/Login.jsx
@@ -1,6 +1,5 @@
 import img2 from "../assets/login-background.png";
 import { useAuthContext } from "../context/AuthContextProvider";
-import Card from 'react-bootstrap';
 
 const sidebarBackground = {
 	backgroundImage: "url(" + img2 + ")",
@@ -14,45 +13,47 @@ const Login = () => {
 	const { setRegisterEmail, setRegisterPassword, setLoginEmail, setLoginPassword, register, login, logout, user } = useAuthContext();
 
 	return (
-		<div>
-			<div
-				style={sidebarBackground}
-			>
-				<div className="m-5">
-					<div>
-						<h3>Register with email and password</h3>
-					</div>
-					<div className="d-flex">
-						<input placeholder="email..." onChange={(event) => { setRegisterEmail(event.target.value) }} />
-						<input placeholder="password..." onChange={(event) => { setRegisterPassword(event.target.value) }} />
-						<button className="btn btn-success" onClick={register}>Create User</button>
-					</div>
+		<div
+			style={sidebarBackground}
+		>
+			<card className="m-3 p-2">
+				<div>
+					<h3>Register with email and password</h3>
 				</div>
-				<div className="m-5">
-					<div>
-						<h3>Login with email and password</h3>
-					</div>
-					<div className="d-flex">
-						<input placeholder="email..." onChange={(event) => { setLoginEmail(event.target.value) }} />
-						<input placeholder="password..." onChange={(event) => { setLoginPassword(event.target.value) }} />
-						<button className="btn btn-light" onClick={login}>Login</button>
-					</div>
+				<div className="p-2">
+					<input placeholder="email..." onChange={(event) => { setRegisterEmail(event.target.value) }} />
 				</div>
-				<div className="m-5">
-					<div>
-						<h3>User logged in</h3>
-						{user?.email}
-					</div>
+				<div className="p-2">
+					<input placeholder="password..." onChange={(event) => { setRegisterPassword(event.target.value) }} />
 				</div>
-				<div className="m-5">
-					<div>
-						<h3>Logout</h3>
-					</div>
-					<div className="d-flex">
-						<button className="btn btn-secondary" onClick={logout}>Sign out</button>
-					</div>
+				<div className="p-2">
+					<button className="btn btn-success" onClick={register}>Create User</button>
 				</div>
-			</div>
+				<div>
+					<h3>Sign in with email and password</h3>
+				</div>
+				<div className="p-2">
+					<input placeholder="email..." onChange={(event) => { setLoginEmail(event.target.value) }} />
+				</div>
+				<div className="p-2">
+					<input placeholder="password..." onChange={(event) => { setLoginPassword(event.target.value) }} />
+				</div>
+				<div className="p-2">
+					<button className="btn btn-primary" onClick={login}>Sign in</button>
+				</div>
+				<div>
+					<h3>User Signed in</h3>
+				</div>
+				<div>
+					{user?.email}
+				</div>
+				<div>
+					<h3>Sign Out</h3>
+				</div>
+				<div className="p-2">
+					<button className="btn btn-secondary" onClick={logout}>Sign out</button>
+				</div>
+			</card>
 		</div>
 	);
 };
