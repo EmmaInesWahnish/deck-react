17e4ef324718c1bfb60bc95c1552512a512dad32
Emma <emma.wahnish@groovinads.com>
2022-04-19 10:26:14 -0300

    config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDbHbFVTPReq7Xv63r8DzNiXNHi7uQmzD8",
-  authDomain: "deck-react.firebaseapp.com",
-  projectId: "deck-react",
-  storageBucket: "deck-react.appspot.com",
-  messagingSenderId: "1019660936735",
-  appId: "1:1019660936735:web:d46bd0e5f6706a9f66972a",
-  measurementId: "G-7QPWH9RLPS"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
